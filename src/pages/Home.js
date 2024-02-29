4ecf207f5c64c744c749c4d628c1269b850a6b05
shivam <shivam.techhira@gmail.com>
2024-02-29 16:18:07 +0530

    Box fixing for home screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -87,30 +87,24 @@ const Home = () => {
           </Col>
         </Row>
         <div>
-          <Row className='hed3'>
-            <Col className='col1' sm={{span: 12}} md={{span: 8}}>
-              <Box height={300} width={350} sx={{ borderRadius: 3, bgcolor: '#0B0F17' }}>
+          <Row className='home_box'>
+            <Col className='home_Col1' sm={{span: 12}} md={{span: 6}} lg={{span:4}}>
                 <img src={Frame} alt="frm1" />
                 <h2>Mobile App & Web Portal</h2>
                 <p>Enjoy the flexibility that comes with having access to an advanced Web portal as well as a mobile
                   App. Post your Jobs from wherever you want. Home, Office, Car, you name it!...</p>
-              </Box>
             </Col>
-            <Col className='col2' sm={{span: 12}} md={{span: 8}}>
-              <Box height={300} width={350} sx={{ borderRadius: 3, bgcolor: '#0B0F17' }}>
+            <Col className='home_Col2' sm={{span: 12}} md={{span: 6}} lg={{span:4}}>
                 <img src={Frame2} alt="frm2" />
                 <h2>Easily Post Jobs Loads</h2>
                 <p>Our user-friendly interface and advanced features make it simple
                   for you to post jobs, stay organized and enhance productivity</p>
-              </Box>
             </Col>
-            <Col className='col3' sm={{span: 12}} md={{span: 8}}>
-              <Box height={300} width={350} sx={{ borderRadius: 3, bgcolor: '#0B0F17' }}>
+            <Col className='home_Col3' sm={{span: 12}} md={{span: 6}} lg={{span:4}}>
                 <img src={Frame3} alt="frm3" />
                 <h2>Pin Point Carrier Search</h2>
                 <p>We are taking the guesswork out of brokerage! Our advanced search features will help you find
                   available carriers with the highest level of precision imaginable, to ensure timely delivery...</p>
-              </Box>
             </Col>
           </Row>
           <Row className='butt2'>
@@ -130,31 +124,25 @@ const Home = () => {
           </Col>
         </Row>
         <div >
-          <Row className='hed4'>
-            <Col className='col4' sm={{span: 12}} md={{span: 8}}>
-              <Box height={300} width={350} sx={{ borderRadius: 3, bgcolor: '#0B0F17' }}>
+          <Row className='home_box2'>
+            <Col className='home_Col4' sm={{span: 12}} md={{span: 6}} lg={{span:4}}>
                 <img src={Frame4} alt="frm4" />
                 <h2>Easily Search Profitable Loads</h2>
                 <p>With our growing network of brokers all over the nation, the opportunities are endless. Our highly
                   customizable job searching features will land you the right jobs by just a few clicks.</p>
-              </Box>
             </Col>
-            <Col className='col5' sm={{span: 12}} md={{span: 8}}>
-              <Box height={300} width={350} sx={{ borderRadius: 3, bgcolor: '#0B0F17' }}>
+            <Col className='home_Col5' sm={{span: 12}} md={{span: 6}} lg={{span:4}}>
                 <img src={Frame5} alt="frm5" />
                 <h2>Efficiently Manage Multiple Drivers</h2>
                 <p>Managing multiple drivers has never been easier, One interface, several
                   drivers! Our driver management features will help you become more
                   efficient with handling multiple drivers at the same time...</p>
-              </Box>
             </Col>
-            <Col className='col6' sm={{span: 12}} md={{span: 8}}>
-              <Box height={300} width={350} sx={{ borderRadius: 3, bgcolor: '#0B0F17' }}>
+            <Col className='home_Col6' sm={{span: 12}} md={{span: 6}} lg={{span:4}}>
                 <img src={Frame6} alt="frm6" />
                 <h2>Efficiently Manage Loads</h2>
                 <p>Experience the power of our load management features that will enable you to
                   easily acquire loads from brokers,sign necessary contracts and pass them on to your drivers by just a click.</p>
-              </Box>
             </Col>
           </Row>
           <Row className='butt3'>
@@ -174,30 +162,24 @@ const Home = () => {
           </Col>
         </Row>
         <div>
-          <Row className='hed5'>
-            <Col className='col7' sm={{span: 12}} md={{span: 8}}>
-              <Box height={300} width={350} sx={{ borderRadius: 3, bgcolor: '#0B0F17' }}>
+          <Row className='home_box3'>
+            <Col className='home_Col7' sm={{span: 12}} md={{span: 6}} lg={{span:4}}>
                 <img src={Frame7} alt="frm7" />
                 <h2>See and be Seen!</h2>
                 <p>Our advanced GPS based driver locator features will always show brokers in our
                   network, where you are, and you will never miss a job! All you need to do is to turn your availability button on...</p>
-              </Box>
             </Col>
-            <Col className='col8' sm={{span: 12}} md={{span: 8}}>
-              <Box height={300} width={350} sx={{ borderRadius: 3, bgcolor: '#0B0F17' }}>
+            <Col className='home_Col8' sm={{span: 12}} md={{span: 6}} lg={{span:4}}>
                 <img src={Frame8} alt="frm8" />
                 <h2>Search for Jobs</h2>
                 <p>Take Control of your routes!  don't be stuck in the middle of nowhere, waiting and
                   hoping for your dispatcher to search through thousands of jobs, until they find you a gig. Search for jobs...</p>
-              </Box>
             </Col>
-            <Col className='col9' sm={{span: 12}} md={{span: 8}}>
-              <Box height={300} width={350} sx={{ borderRadius: 3, bgcolor: '#0B0F17' }}>
+            <Col className='home_Col9' sm={{span: 12}} md={{span: 6}} lg={{span:4}}>
                 <img src={Frame9} alt="frm9" />
                 <h2>Scan, Sign and Store Documents</h2>
                 <p>Forget the old days of running to the print shop to scan and sign important documents.
                   Now you can save your valuable time with our document management features. Scan...</p>
-              </Box>
             </Col>
           </Row>
           <Row className='butt3'>
